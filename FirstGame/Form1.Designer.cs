a6289d50c4e462c0b1a406143df10d0b998e9fe6
su-pr007 <maksimka.suvorov.01@list.ru>
2020-11-11 17:56:10 +0300

    first commit

---
+++ FirstGame/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FirstGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MoveBgTimer = new System.Windows.Forms.Timer(this.components);
+            this.mainPlayer = new System.Windows.Forms.PictureBox();
+            this.LeftMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.RightMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.UpMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.DownMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveBulletsTimer = new System.Windows.Forms.Timer(this.components);
+            this.ShootingTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveEnemiesTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MoveBgTimer
+            // 
+            this.MoveBgTimer.Enabled = true;
+            this.MoveBgTimer.Interval = 10;
+            this.MoveBgTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // mainPlayer
+            // 
+            this.mainPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.mainPlayer.Image = global::FirstGame.Properties.Resources.cowboy;
+            this.mainPlayer.Location = new System.Drawing.Point(548, 484);
+            this.mainPlayer.Name = "mainPlayer";
+            this.mainPlayer.Size = new System.Drawing.Size(100, 100);
+            this.mainPlayer.TabIndex = 0;
+            this.mainPlayer.TabStop = false;
+            // 
+            // LeftMoveTimer
+            // 
+            this.LeftMoveTimer.Interval = 10;
+            this.LeftMoveTimer.Tick += new System.EventHandler(this.LeftMoveTimer_Tick);
+            // 
+            // RightMoveTimer
+            // 
+            this.RightMoveTimer.Interval = 10;
+            this.RightMoveTimer.Tick += new System.EventHandler(this.RightMoveTimer_Tick);
+            // 
+            // UpMoveTimer
+            // 
+            this.UpMoveTimer.Interval = 10;
+            this.UpMoveTimer.Tick += new System.EventHandler(this.UpMoveTimer_Tick);
+            // 
+            // DownMoveTimer
+            // 
+            this.DownMoveTimer.Interval = 10;
+            this.DownMoveTimer.Tick += new System.EventHandler(this.DownMoveTimer_Tick);
+            // 
+            // MoveBulletsTimer
+            // 
+            this.MoveBulletsTimer.Enabled = true;
+            this.MoveBulletsTimer.Interval = 10;
+            this.MoveBulletsTimer.Tick += new System.EventHandler(this.MoveBulletsTimer_Tick);
+            // 
+            // ShootingTimer
+            // 
+            this.ShootingTimer.Tick += new System.EventHandler(this.shootingTimer_Tick);
+            // 
+            // MoveEnemiesTimer
+            // 
+            this.MoveEnemiesTimer.Enabled = true;
+            this.MoveEnemiesTimer.Interval = 10;
+            this.MoveEnemiesTimer.Tick += new System.EventHandler(this.MoveEnemiesTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FirstGame.Properties.Resources.bgimage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.mainPlayer);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "Form1";
+            this.Text = "2D Redemption";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MoveBgTimer;
+        private System.Windows.Forms.PictureBox mainPlayer;
+        private System.Windows.Forms.Timer LeftMoveTimer;
+        private System.Windows.Forms.Timer RightMoveTimer;
+        private System.Windows.Forms.Timer UpMoveTimer;
+        private System.Windows.Forms.Timer DownMoveTimer;
+        private System.Windows.Forms.Timer MoveBulletsTimer;
+        private System.Windows.Forms.Timer ShootingTimer;
+        private System.Windows.Forms.Timer MoveEnemiesTimer;
+    }
+}
+
---
+++ FirstGame/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WMPLib;
+
+namespace FirstGame
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] cloud;
+        int backgroundSpeed;
+        Random rand;
+        int playerSpeed;
+
+        PictureBox[] bullets;
+        int bulletSpeed;
+
+        PictureBox[] enemies;
+        int enemiesSpeed;
+
+        WindowsMediaPlayer Shoot;
+        WindowsMediaPlayer GameSong;
+        WindowsMediaPlayer DeathSound;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for(int i = 0; i < cloud.Length; i++)
+            {
+                cloud[i].Left += backgroundSpeed;
+                if (cloud[i].Left >= 1280)
+                {
+                    cloud[i].Left = -cloud[i].Width;
+                }
+            }
+            for(int i = cloud.Length; i < cloud.Length; i++)
+            {
+                cloud[i].Left += backgroundSpeed - 10;
+                if(cloud[i].Left >= 1280)
+                {
+                    cloud[i].Left = cloud[i].Left;
+                }
+            }
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundSpeed = 5;
+            cloud = new PictureBox[20];
+            rand = new Random();
+            playerSpeed = 3;
+
+            bullets = new PictureBox[1];
+            bulletSpeed = 80;
+
+            enemies = new PictureBox[4];
+            int sizeEnemy = rand.Next(60, 80);
+            enemiesSpeed = 3;
+
+            Image easyEnemies = Image.FromFile("assets\\virus.gif");
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i] = new PictureBox
+                {
+                    Size = new Size(sizeEnemy, sizeEnemy),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    BackColor = Color.Transparent,
+                    Image = easyEnemies,
+                    Location = new Point((i + 1) * rand.Next(90, 160) + 1080, rand.Next(450, 600))
+                };
+
+                this.Controls.Add(enemies[i]);
+            }
+
+
+            Shoot = new WindowsMediaPlayer
+            {
+                URL = "songs\\shoot.wav"
+            };
+            Shoot.settings.volume = 5;
+
+
+            DeathSound = new WindowsMediaPlayer
+            {
+                URL = "songs\\DeathSound.wav"
+            };
+            DeathSound.settings.volume = 10;
+
+
+            GameSong = new WindowsMediaPlayer
+            {
+                URL = "songs\\bgmusic.mp3"
+            };
+            GameSong.settings.setMode("loop", true);
+            GameSong.settings.volume = 15;
+
+            GameSong.controls.play();
+
+            for (int i = 0; i < bullets.Length; i++)
+            {
+                bullets[i] = new PictureBox
+                {
+                    BorderStyle = BorderStyle.None,
+                    Size = new Size(20, 5),
+                    BackColor = Color.White
+                };
+
+                this.Controls.Add(bullets[i]);
+            }
+            
+
+            for(int i = 0; i < cloud.Length; i++)
+            {
+                cloud[i] = new PictureBox
+                {
+                    BorderStyle = BorderStyle.None,
+                    Location = new Point(rand.Next(-1000, 1280), rand.Next(100, 380))
+                };
+                if (i % 2 == 1)
+                {
+                    cloud[i].Size = new Size(rand.Next(100, 225), rand.Next(30, 70));
+                }
+                else
+                {
+                    cloud[i].Size = new Size(150, 25);
+                }
+                cloud[i].BackColor = Color.FromArgb(rand.Next(50, 125), 255, 205, 205);
+                this.Controls.Add(cloud[i]);
+            }
+        }
+
+        private void LeftMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if(mainPlayer.Left > 10)
+            {
+                mainPlayer.Left -= playerSpeed;
+            }
+        }
+
+        private void RightMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (mainPlayer.Left < 1150)
+            {
+                mainPlayer.Left += playerSpeed;
+            }
+
+        }
+
+        private void UpMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (mainPlayer.Top > 10)
+            {
+                mainPlayer.Top -= playerSpeed;
+            }
+        }
+
+        private void DownMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (mainPlayer.Top < 600)
+            {
+                mainPlayer.Top += playerSpeed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left || e.KeyCode == Keys.Right || e.KeyCode == Keys.Down || e.KeyCode == Keys.Up)
+            {
+                mainPlayer.Image = Properties.Resources.cowboy_run;
+                if (e.KeyCode == Keys.Left)
+                {
+                    LeftMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Right)
+                {
+                    RightMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Up)
+                {
+                    UpMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    DownMoveTimer.Start();
+                }
+            }
+            if(e.KeyCode == Keys.Space)
+            {
+                ShootingTimer.Start();
+            }
+            if(e.KeyCode == Keys.Escape)
+            {
+                Process.GetCurrentProcess().Kill();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right || e.KeyCode == Keys.Down || e.KeyCode == Keys.Up)
+            {
+                mainPlayer.Image = Properties.Resources.cowboy;
+                if (e.KeyCode == Keys.Left)
+                {
+                    LeftMoveTimer.Stop();
+                }
+                if (e.KeyCode == Keys.Right)
+                {
+                    RightMoveTimer.Stop();
+                }
+                if (e.KeyCode == Keys.Up)
+                {
+                    UpMoveTimer.Stop();
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    DownMoveTimer.Stop();
+                }
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                ShootingTimer.Stop();
+            }
+
+        }
+
+        private void MoveBulletsTimer_Tick(object sender, EventArgs e)
+        {
+            for(int i = 0; i < bullets.Length; i++)
+            {
+                bullets[i].Left += bulletSpeed;
+            }
+        }
+
+        private void shootingTimer_Tick(object sender, EventArgs e)
+        {
+            Shoot.controls.play();
+            for (int i = 0; i < bullets.Length; i++)
+            {
+                if (bullets[i].Left > 1280)
+                {
+                    bullets[i].Location = new Point(mainPlayer.Location.X + 100 + (i * 50), mainPlayer.Location.Y + 50);
+                }
+            }
+
+            Intersect();
+        }
+
+        private void MoveEnemiesTimer_Tick(object sender, EventArgs e)
+        {
+            MoveEnemies(enemies, enemiesSpeed);
+        }
+        private void MoveEnemies(PictureBox[] enemies, int speed)
+        {
+            for(int i=0; i<enemies.Length; i++)
+            {
+                enemies[i].Left -= speed + (int)(Math.Sin(enemies[i].Left * Math.PI / 180) + Math.Cos(enemies[i].Left * Math.PI / 180));
+
+                Intersect();
+
+                if(enemies[i].Left < this.Left)
+                {
+                    int sizeEnemy = rand.Next(60, 90);
+                    enemies[i].Size = new Size(sizeEnemy, sizeEnemy);
+                    enemies[i].Location = new Point((i + 1) * rand.Next(150, 250) + 1080, rand.Next(450, 650));
+                }
+            }
+        }
+        private void Intersect()
+        {
+            for(int i = 0; i < enemies.Length; i++)
+            {
+                if (bullets[0].Bounds.IntersectsWith(enemies[i].Bounds))
+                {
+                    enemies[i].Location = new Point(rand.Next(150, 250) + 1280, rand.Next(450, 600));
+                    bullets[0].Location = new Point(2000, mainPlayer.Location.Y + 50);
+                    DeathSound.controls.play();
+                }
+                if (mainPlayer.Bounds.IntersectsWith(enemies[i].Bounds))
+                {
+                    mainPlayer.Visible = false;
+                    Process.GetCurrentProcess().Kill();
+                }
+            }
+        }
+    }
+}
